e715cca6428e3983d86f9fd1453973c21a72f6cb
NeverSettels <settels.eric@gmail.com>
2019-07-30 15:28:48 -0500

    getting deploy to work

---
+++ backend/public/build/precache-manifest.dd31f7af89181ebfdef531bc2dabe8fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7606cf2ca4a7c81de06b52977f9ce7c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b16fadb4d95784a876d5",
+    "url": "/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "05e933c09e27b22e68ef",
+    "url": "/static/css/main.e5df59ed.chunk.css"
+  },
+  {
+    "revision": "b16fadb4d95784a876d5",
+    "url": "/static/js/2.d36b0b89.chunk.js"
+  },
+  {
+    "revision": "05e933c09e27b22e68ef",
+    "url": "/static/js/main.96e6d1ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
