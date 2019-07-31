f0a7dbc0c4cc0a8b40d431dcc733720de5c17b90
NeverSettels <settels.eric@gmail.com>
2019-07-31 01:24:44 -0500

    fixed  comment routes some more

--- backend/controllers/commentControllers.js
+++ backend/controllers/commentControllers.js
@@ -3,18 +3,18 @@ const Comment = require('../models/Comment')
 exports.getComments = (req, res, next) => {
   const { id } = req.params
   Comment.find({ postedOn: id })
-    .then(posts => res.status(200).json({ posts }))
+    .then(comments => res.status(200).json({ comments }))
     .catch(err => res.status(500).json(err))
 }
 exports.createComment = (req, res, next) => {
   Comment.create({ ...req.body })
-    .then(draft => res.status(201).json({ draft }))
+    .then(comment => res.status(201).json({ comment }))
     .catch(err => res.status(500).json(err))
 }
 
 exports.updateComment = (req, res, next) => {
   const { id } = req.params
   Comment.findByIdAndUpdate(id, { ...req.body }, { new: true })
-    .then(post => res.status(200).json({ post }))
+    .then(comment => res.status(200).json({ comment }))
     .catch(err => res.status(500).json(err))
 }
